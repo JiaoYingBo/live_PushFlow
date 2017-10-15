844051ed7994d19e04129628aca8f301cedcf894
JiaoYingBo <715775799@qq.com>
2017-10-15 15:08:00 +0800

    fix

--- live_推流/使用LFLiveKit推流/使用LFLiveKit推流/ViewController.m
+++ live_推流/使用LFLiveKit推流/使用LFLiveKit推流/ViewController.m
@@ -24,7 +24,7 @@
 
 - (IBAction)start:(id)sender {
     LFLiveStreamInfo *stream = [[LFLiveStreamInfo alloc] init];
-    stream.url = @"rtmp://192.168.0.101/live/test";
+    stream.url = @"rtmp://192.168.0.101:1990/liveApp/room";
     [self.session startLive:stream];
     [self.session setRunning:YES];
 }
